dca81c8ed356d9cca4a6605a9a8e1d6606779eda
Dyhoereos <dennis.chau@outlook.com>
2016-12-01 23:07:29 -0800

    Added sliders to change logo view

--- CALayerPlayground/CALayerPlayground/FirstViewController.swift
+++ CALayerPlayground/CALayerPlayground/FirstViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class FirstViewController: UIViewController {
  
     @IBOutlet weak var viewForLayer: UIView!
+    @IBOutlet weak var gravitySlider: UISlider!
+    @IBOutlet weak var cornersSlider: UISlider!
+    @IBOutlet weak var shadowSlider: UISlider!
+    @IBOutlet weak var borderSlider: UISlider!
     
     var l: CALayer {
         return viewForLayer.layer
@@ -26,19 +30,72 @@ class FirstViewController: UIViewController {
     }
     
     func setUpLayer() {
-        l.backgroundColor = UIColor.blue.cgColor
-        l.borderWidth = 100.0
-        l.borderColor = UIColor.red.cgColor
+        l.backgroundColor = UIColor(red: 0.0, green: 62/255.0, blue: 126/255.0, alpha: 1.0).cgColor
+        l.borderWidth = 10.0
+        l.borderColor = UIColor(red: 0.0, green: 136/255.0, blue: 82/255.0, alpha: 1.0).cgColor
         l.shadowOpacity = 0.7
         l.shadowRadius = 10.0
-        l.contents = UIImage(named: "first")?.cgImage
-        l.contentsGravity = kCAGravityCenter
+        l.contents = UIImage(named: "hockeystick")?.cgImage
+        l.masksToBounds = false
+        l.contentsGravity = kCAGravityRight
+        l.cornerRadius = 30
+        
+
+        
     }
 
     @IBAction func tappedGesture(_ sender: UITapGestureRecognizer) {
+        l.shadowOpacity = l.shadowOpacity == 0.7 ? 0.0 : 0.7
     }
 
     @IBAction func pinchedGesture(_ sender: UIPinchGestureRecognizer) {
+        let offset: CGFloat = sender.scale < 1 ? 5.0 : -5.0
+        let oldFrame = l.frame
+        let oldOrigin = oldFrame.origin
+        let newOrigin = CGPoint(x: oldOrigin.x + offset, y: oldOrigin.y + offset)
+        let newSize = CGSize(width: oldFrame.width + (offset * -2.0), height: oldFrame.height + (offset * -2.0))
+        let newFrame = CGRect(origin: newOrigin, size: newSize)
+        if newFrame.width >= 150.0 && newFrame.width <= 300.0 {
+            l.borderWidth -= offset
+            l.cornerRadius += (offset / 2.0)
+            l.frame = newFrame
+        }
     }
+    @IBAction func gravitySlide(_ sender: UISlider) {
+        sender.setValue(Float(lroundf(gravitySlider.value)), animated: true)
+        print(sender.value)
+        switch(sender.value){
+        case 1: l.contentsGravity = kCAGravityCenter
+        break;
+        case 2: l.contentsGravity = kCAGravityTop
+        break;
+        case 3: l.contentsGravity = kCAGravityBottom
+        break;
+        case 4: l.contentsGravity = kCAGravityLeft
+        break;
+        case 5: l.contentsGravity = kCAGravityRight
+        break;
+        case 6: l.contentsGravity = kCAGravityTopLeft
+        break;
+        case 7: l.contentsGravity = kCAGravityTopRight
+        break;
+        case 8: l.contentsGravity = kCAGravityBottomLeft
+        break;
+        case 9: l.contentsGravity = kCAGravityBottomRight
+        break;
+        default: l.contentsGravity = kCAGravityRight
+        break;
+        }
+    }
+    @IBAction func borderSlide(_ sender: UISlider) {
+    }
+    @IBAction func shadowSlide(_ sender: UISlider) {
+    }
+    @IBAction func cornerSlide(_ sender: UISlider) {
+    }
+    
+    
+    
+    
 }
 
